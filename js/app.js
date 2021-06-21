fdfb41db46326bc20bc71ab0ae37f8bbc7f21d1f
Sahar Zal <68024175+saharzal@users.noreply.github.com>
2021-06-21 12:27:50 +0430

    delete comments of app.js

--- js/app.js
+++ js/app.js
@@ -18,22 +18,3 @@ btns.forEach(element =>{
         } 
     });
 });
-// btns.addEventListener('click',()=>{
-    
-//     btn.classList.toggle('active');
-//     // if(content.style.display == 'block')
-//     //     content.style.display = 'none';
-//     // else 
-//     //     content.style.display = 'block';
-//     if (content.style.maxHeight){
-//         content.style.maxHeight = null;
-//         openIcon.style.display = 'block';
-//         closeIcon.style.display = 'none';
-//         console.log('close');
-//     } else {
-//         console.log('open');
-//         content.style.maxHeight = content.scrollHeight + "px";
-//         openIcon.style.display = 'none';
-//         closeIcon.style.display = 'block';
-//     } 
-// });
